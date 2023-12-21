cb23fb2e08a6cb31f2c093dfc77704fa0508db45
INB-RTBritos <154307214+INB-RTBritos@users.noreply.github.com>
2023-12-21 10:53:17 +0800

    Add2Values_Function.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 int Add2Values(int, int);
@@ -7,16 +6,14 @@ int Add2Values(int, int);
 int main()
 {
     int x, y;
-    cout
-        << "-- Add 2 Values --\n"
-        << "Enter x Values: ";
+    cout << "-- Add 2 Values --\n" << "Enter x Values: ";
     cin >> x;
     cout << "Enter y Value: ";
     cin >> y;
 
-    cout << "__________________";
+    cout << "When you add the 2 Values the result is: ";
 
-    cout << Add2Values(x, y)
+    cout << Add2Values(x, y);
 
     return 0;
 }
@@ -24,4 +21,8 @@ int main()
 int Add2Values(int x, int y)
 {
     // make your code here
+
+    int sum;
+    sum = x + y;
+    return sum;
 }
